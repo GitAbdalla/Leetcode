7f97afbfe89a34017cc30373accee176ffa7f096
Abdalla Hassan <90405765+GitAbdalla@users.noreply.github.com>
2024-07-14 16:24:35 +0300

    [125] [Time Beats: 30.56%] [Memory Beats: 40.14%] - LeetPush

--- 125-Valid-Palindrome.js
+++ 125-Valid-Palindrome.js
@@ -3,11 +3,8 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-     s = s.replace(/[^a-zA-Z0-9]/g,"").toLowerCase()
-    let reverseS =""
-    for (let i = s.length -1; i >=0; i--){
-        reverseS +=s[i]
-    }
+    let s2 = s.replace(/[^a-zA-Z0-9]/g,"").toLowerCase()
+    let reverseS = s2.split("").reverse().join("")
    
-    return s === reverseS
+    return s2 === reverseS
 };
