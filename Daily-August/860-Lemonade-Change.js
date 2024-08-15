c9336dd613532539fd815cee7afd433f319a8bf5
Abdalla Hassan <90405765+GitAbdalla@users.noreply.github.com>
2024-08-15 18:20:30 +0300

    [860] [Time Beats: 15.74%] [Memory Beats: 89.34%] - LeetPush

---
+++ Daily-August/860-Lemonade-Change.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function (bills) {
+    let fiveCount = 0
+    let tenCount = 0
+
+    for (let i = 0; i < bills.length; i++) {
+        if (bills[i] === 5) {
+            fiveCount++
+        } else if (bills[i] === 10) {
+            if (fiveCount > 0) {
+                fiveCount--
+                tenCount++
+            } else {
+                return false
+            }
+        } else if (bills[i] === 20) {
+            if (tenCount > 0 && fiveCount > 0) {
+                tenCount--
+                fiveCount--
+            } else if (fiveCount >= 3) {
+                fiveCount -= 3
+            } else {
+                return false
+            }
+
+        }
+    }
+    return true
+
+};
