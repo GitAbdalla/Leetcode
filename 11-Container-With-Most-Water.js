cb2b198261aeffca2e4a2e2db6e20f08bc21bd73
Abdalla Hassan <90405765+GitAbdalla@users.noreply.github.com>
2024-09-10 01:07:31 +0300

    [11] [Time Beats: 52.16%] [Memory Beats: 8.53%] - LeetPush

---
+++ 11-Container-With-Most-Water.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let left = 0
+    let right = height.length - 1
+    let maxArea = 0
+    while (left < right) {
+        let currentArea = ((right - left) * Math.min(height[left], height[right]))
+        
+        maxArea = Math.max(maxArea, currentArea)
+
+        if (height[left] < height[right]) {
+            left++
+        } else {
+            right--
+        }
+    }
+    return maxArea
+};
