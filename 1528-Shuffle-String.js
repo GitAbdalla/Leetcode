ee61b1741a4a7962f7e009d4a4ffeffa1a612483
Abdalla Hassan <90405765+GitAbdalla@users.noreply.github.com>
2024-09-15 02:40:00 +0300

    [1528] [Time Beats: 89.97%] [Memory Beats: 88.18%] - LeetPush

---
+++ 1528-Shuffle-String.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function (s, indices) {
+    let string = []
+
+    for (let i = 0; i < indices.length; i++) {
+        string[indices[i]] = s[i]
+    }
+    return string.join("")
+};
